b04d22d41f0d82586654d2c0385fdf56ce894224
markmtran <markmtran1@gmail.com>
2024-03-18 13:16:56 -0700

    changed paths for Colab

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorlayer.layers import *
 
-w_init = tf.variance_scaling_initializer()
+w_init = tf.initializers.variance_scaling()
 a_init = tf.constant_initializer(0.2)
 
 # VGG for perceptual loss
