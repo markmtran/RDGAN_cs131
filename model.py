8d009221911089b136af19d029f788f7f5e6e8eb
markmtran <markmtran1@gmail.com>
2024-03-18 20:00:51 -0700

    changes for Colab

--- model.py
+++ model.py
@@ -150,8 +150,8 @@ def wtv_loss(img_v, img_i, size):
 def close_op(img_v):
     # out = tf.layers.max_pooling2d(img_v, 3, 1, padding='same')
     # return -tf.layers.max_pooling2d(-out, 3, 1, padding='same')
-    out = tf.keras.layers.MaxPooling2D(3, 1, padding='same')(img_v)
-    return -tf.keras.layers.MaxPooling2D(3, 1, padding='same')(out)
+    out = tf.keras.layers.MaxPooling2D(pool_size=3, strides=1, padding='same')(img_v)
+    return -tf.keras.layers.MaxPooling2D(pool_size=3, strides=1, padding='same')(out)
 
 # stretch for I
 def stretch(img_v, img_i):
